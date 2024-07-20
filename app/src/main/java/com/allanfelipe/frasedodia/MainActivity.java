a76ddbee134b4582da3dc0eeba161437fc4e901f
Allan <allanfelipedk@gmail.com>
2024-07-19 21:13:50 -0300

    feat:finishing the APP - phrase of the day

--- app/src/main/java/com/allanfelipe/frasedodia/MainActivity.java
+++ app/src/main/java/com/allanfelipe/frasedodia/MainActivity.java
@@ -1,24 +1,45 @@
 package com.allanfelipe.frasedodia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] frases = {
+            "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+            "Acredite que você pode, assim você já está no meio do caminho.",
+            "A única maneira de fazer um ótimo trabalho é amar o que você faz.",
+            "Não conte os dias, faça os dias contarem.",
+            "A vida é 10% o que acontece com você e 90% como você reage a isso.",
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    public void gerarFrase(View view) {
+        TextView texto = findViewById(R.id.textResultado);
+        int numeroAleatorio = new Random().nextInt(10);
+        String frase = frases[numeroAleatorio];
+        texto.setText(frase);
+    }
+
+    public void exibirTodas(View view) {
+        TextView texto = findViewById(R.id.textResultado);
+        String textoResultado = "";
+
+        for (String frase : frases) {
+            textoResultado = textoResultado + frase + "\n";
+        }
+        texto.setText(textoResultado);
+    }
+
 }
